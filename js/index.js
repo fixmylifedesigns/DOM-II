dfb803fef622a04db1b2ad98754ea1bbc67ffa50
Irving Duran <ijd.irving@gmail.com>
2019-03-05 14:58:30 -0400

    buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+//buttons
+let btn = document.querySelectorAll(".btn");
+console.log(btn[0]);
+
+const signUpBtn1 = btn[0];
+
+// buttton 1
+signUpBtn1.addEventListener("mouseenter", function(btn1) {
+  console.log(`signUpBtn fired!`);
+  btn1.target.style.color = "blue";
+  btn1.target.style.background = "black";
+});
+signUpBtn1.addEventListener("mouseleave", function(btn1) {
+  console.log(`signUpBtn fired!`);
+  btn1.target.style.color = "white";
+});
+
+// buttton 2
+const signUpBtn2 = btn[1];
+signUpBtn2.addEventListener("mouseenter", function(btn2) {
+  console.log(`signUpBtn fired!`);
+  btn2.target.style.color = "red";
+  btn2.target.style.background = "black";
+});
+signUpBtn2.addEventListener("mouseleave", function(btn2) {
+  console.log(`signUpBtn fired!`);
+  btn2.target.style.color = "white";
+});
+
+// buttton 3
+const signUpBtn3 = btn[2];
+signUpBtn3.addEventListener("mouseenter", function(btn3) {
+  console.log(`signUpBtn fired!`);
+  btn3.target.style.color = "green";
+  btn3.target.style.background = "black";
+});
+signUpBtn3.addEventListener("mouseleave", function(btn3) {
+  console.log(`signUpBtn fired!`);
+  btn3.target.style.color = "white";
+});
